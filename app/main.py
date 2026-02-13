de0843da5f70b6cd48062ee5601d3390d2657451
Andrew P <andrew.pischchany@gmail.com>
2026-02-13 20:47:12 +0100

    Update cinema visit app structure and workflows

--- app/main.py
+++ app/main.py
@@ -1,6 +1,32 @@
-# write your imports here
+from app.cinema.hall import CinemaHall
+from app.cinema.bar import CinemaBar
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+    customers: list[dict[str, str]],
+    hall_number: int,
+    cleaner: str,
+    movie: str,
+) -> None:
+    customer_list = []
+    for customer in customers:
+        customer_list.append(
+            Customer(
+                name=customer["name"],
+                food=customer["food"],
+            )
+        )
+
+    hall = CinemaHall(hall_number)
+    cleaner_instance = Cleaner(name=cleaner)
+
+    for customer in customer_list:
+        CinemaBar.sell_product(product=customer.food, customer=customer)
+
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_list,
+        cleaning_staff=cleaner_instance,
+    )
